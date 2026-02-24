560b6512fcd5899461addb34a490a3c4af9e4c02
arend <arendmoerman@gmail.com>
2026-02-24 12:41:26 +0100

    Updated documentation generation instructions for developers

---
+++ docs/search/all_e.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['objects_0',['Objects',['../classPyPO_1_1Enums_1_1Objects.html',1,'PyPO::Enums']]],
+  ['ochief_1',['oChief',['../structRTDictf.html#a8e68218abe120199a50d26cec14f98fa',1,'RTDictf::oChief()'],['../structGRTDictf.html#a415e6900c4f83fe28d745f56c3790ba6',1,'GRTDictf::oChief()']]],
+  ['optdof_2',['OptDoF',['../classPyPO_1_1Enums_1_1OptDoF.html',1,'PyPO::Enums']]],
+  ['optimising_20reflector_2fgroup_20configurations_20in_20pypo_3',['Optimising reflector/group configurations in PyPO',['../basictut6.html',1,'']]],
+  ['origin_4',['ORIGIN',['../WorldParam_8py.html#a5060a4522156defc526f9f53ba6ea997',1,'PyPO::WorldParam']]]
+];
---
+++ docs/search/pages_5.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['installation_0',['Installation',['../install.html',1,'']]],
+  ['instructions_20for_20contributions_20_26_20development_1',['Instructions For Contributions &amp; Development',['../contribguide.html',1,'']]],
+  ['introduction_20to_20physical_20optics_2',['Introduction To Physical Optics',['../pobasics.html',1,'']]],
+  ['introduction_20to_20pypo_20_26_20design_20principles_3',['Introduction To PyPO &amp; Design Principles',['../basictut1.html',1,'']]]
+];
